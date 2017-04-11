605896e0d2a9ec82417980c1170f3d0d4c86c98a
helenypan <yue.pan@me.com>
2017-04-11 14:35:25 +0100

    modify regex exampleto finish the assignment

--- python-informatics/regex.py
+++ python-informatics/regex.py
@@ -1,18 +1,12 @@
-name = raw_input("Enter file:")
-if len(name) < 1 : name = "mbox-short.txt"
-handle = open(name)
-hr_dict = dict()
-for line in handle:
-	if line.startswith("From "):
-		line_words = line.split()
-		time = line_words[5]
-		hr= time.split(":")[0]
-		hr_dict[hr] = hr_dict.get(hr, 0) + 1
+import re
 
-hr_list = list()
-for hr, count in hr_dict.items():
-	hr_list.append((hr, count))
-hr_list.sort()
-print hr_list
-for hr,count in hr_list:
-	print hr, count
+fname = "regex_sum_368588.txt"
+handle = open(fname)
+num_list = list()
+for line in handle:
+	line = line.rstrip()
+	nums = re.findall('[1-9][0-9]*',line)
+	for num in nums:
+		num_list.append(int(num))
+print sum(num_list)
+
