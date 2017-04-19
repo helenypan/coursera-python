ad42d360e668f26b4010bc7036a0d7a6ea9823ed
helenypan <yue.pan@me.com>
2017-04-19 22:16:50 +0100

    add parameters to constructor in python class example partyAnimal

--- python-informatics/partyAnimal.py
+++ python-informatics/partyAnimal.py
@@ -1,19 +1,26 @@
 class PartyAnimal:
 	x = 0 
+	name = ""
 
-	def __init__(self):
-		print "I am constructed", self.x
+	def __init__(self, name):
+		self.name = name
+		print "I am constructed", self.x, self.name
 
 	def party(self):
 		self.x = self.x + 1
-		print "So far", self.x
+		print self.name, "So far", self.x
 
 	def __del__(self):
-		print "I am destructed", self.x
+		print "I am destructed", self.name, self.x
 
-an = PartyAnimal()
+an = PartyAnimal("Helen")
 an.party()
 an.party()
 an.party()
-print "Type", type(an)
-print "Dir",dir(an)
+
+ab = PartyAnimal("Wudong")
+ab.party()
+ab.party()
+
+# print "Type", type(an)
+# print "Dir",dir(an)
