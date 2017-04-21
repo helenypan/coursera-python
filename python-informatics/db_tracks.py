4641f50d41e089ea35d6c089fcffda01fc6da36c
helenypan <yue.pan@me.com>
2017-04-21 22:59:35 +0100

    update db_tracks script

--- python-informatics/db_tracks.py
+++ python-informatics/db_tracks.py
@@ -1,11 +1,23 @@
 import xml.etree.ElementTree as ET
 import sqlite3
 
-conn = sqlite3.connect("trackdb.sqlite")
+conn = sqlite3.connect("db_tracks.sqlite")
 cur = conn.cursor()
+
+cur.executescript('''
+	DROP TABLE IF EXISTS Artist;
+	DROP TABLE IF EXISTS Album;
+	DROP TABLE IF EXISTS Track;
+	''')
 
 cur.execute('''
 	CREATE TABLE IF NOT EXISTS Artist(
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	name TEXT UNIQUE)
+	''')
+
+cur.execute('''
+	CREATE TABLE IF NOT EXISTS Genre(
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	name TEXT UNIQUE)
 	''')
@@ -22,6 +34,7 @@ cur.execute('''
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	title TEXT UNIQUE,
 	album_id INTEGER,
+	genre_id INTEGER,
 	len INTEGER,
 	rating INTEGER,
 	count INTEGER)
@@ -53,22 +66,30 @@ for entry in all:
 	count = lookup(entry, "Play Count")
 	rating = lookup(entry, "Rating")
 	length = lookup(entry, "Total Time")
+	genre = lookup(entry, "Genre")
 
-	if name is None or artist is None or all is None :
+	if name is None or artist is None or all is None or album is None or genre is None:
 		continue
-	print name, artist, album, count, rating, length
+	print name, artist, album, count, rating, length, genre
 
 	cur.execute('''INSERT OR IGNORE INTO Artist(name)
 		VALUES(?)''', (artist,))
 	cur.execute('SELECT id from Artist WHERE name =?',(artist,))
 	artist_id = cur.fetchone()[0]
 
+	cur.execute('''
+		INSERT OR IGNORE INTO Genre(name)
+		VALUES(?)''', (genre,))
+	cur.execute('SELECT id from Genre WHERE name =?',(genre,))
+	genre_id =cur.fetchone()[0]
+
 	cur.execute('''INSERT OR IGNORE INTO Album(title, artist_id)
 		VALUES(?,?)''', (album, artist_id))
+	print album
 	cur.execute('SELECT id FROM Album where title = ?',(album,))
 	album_id = cur.fetchone()[0]
 
-	cur.execute('''INSERT OR REPLACE INTO Track(title, album_id, len, rating, count)
-		VALUES(?,?,?,?,?)''', (name, album_id, length, rating, count))
+	cur.execute('''INSERT OR REPLACE INTO Track(title, album_id, genre_id, len, rating, count)
+		VALUES(?,?,?,?,?,?)''', (name, album_id, genre_id,length, rating, count))
 
 	conn.commit()
